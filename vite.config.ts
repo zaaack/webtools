102bc4cd93684463759c5d9ec6ce9a5c798b401c
yangzhen <yangzhen@douban.com>
2023-02-25 10:53:33 +0800

    f

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,9 @@ import react from '@vitejs/plugin-react'
 import { VitePWA } from 'vite-plugin-pwa'
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), VitePWA({ registerType: 'autoUpdate' })],
+  plugins: [react(),
+    // VitePWA({ registerType: 'autoUpdate' })
+  ],
   base: './',
   build: {
     outDir: './docs',

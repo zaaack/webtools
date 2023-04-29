7a3f3a9905679cdd8c604040d8438157d691b69b
zaaack <zaaack@qq.com>
2023-04-29 16:00:17 +0800

    f

--- src/main.tsx
+++ src/main.tsx
@@ -1,22 +1,26 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import ColorStrenth from './color-strenth'
-import { Link, Router } from 'react-router-dom'
-import './index.css'
-import { HashRouter, Route, Routes } from 'react-router-dom'
-import { Timer } from './timer'
-import Memo from './memo'
-import { NotePage } from './memo/note/index.quill'
-import { NotePage as TNote } from './memo/note/index'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import ColorStrenth from "./color-strenth";
+import { Link, Router } from "react-router-dom";
+import "./index.css";
+import { List } from "antd-mobile";
+import { HashRouter, Route, Routes } from "react-router-dom";
+import { Timer } from "./timer";
 function Home() {
   return (
-    <div>
-      <Link to="/color-strenth">颜色强度计</Link><br/>
-      <Link to="/timer">计时器</Link><br/>
-      <Link to="/xykey">xykey</Link><br/>
-      <Link to="/memo">memo</Link><br/>
-    </div>
-  )
+    <List>
+      <Link to="/color-strenth">
+        <List.Item>颜色强度计</List.Item>
+      </Link>
+
+      <Link to="/timer">
+        <List.Item>计时器</List.Item>
+      </Link>
+      <Link to="/xykey">
+        <List.Item>xykey</List.Item>
+      </Link>
+    </List>
+  );
 }
 function Main() {
   return (
@@ -26,14 +30,11 @@ function Main() {
           <Route path="/" element={<Home />} />
           <Route path="/color-strenth" element={<ColorStrenth />} />
           <Route path="/timer" element={<Timer />} />
-          <Route path="/memo" element={<Memo />} />
-          <Route path="/memo/note/:id" element={<NotePage />} />
-          <Route path="/tmemo/note/:id" element={<TNote />} />
         </Routes>
       </HashRouter>
     </React.StrictMode>
-  )
+  );
 }
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <Main />
-)
+);
--- src/timer/index.tsx
+++ src/timer/index.tsx
@@ -1,145 +1,156 @@
-import dayjs from 'dayjs'
-import React, { useEffect, useState } from 'react'
-import { store } from './store'
+import dayjs from "dayjs";
+import React, { useEffect, useState } from "react";
+import { store } from "./store";
+import { Button, Checkbox, Picker } from "antd-mobile";
+import css from "./index.module.scss";
 if (window.Notification) {
-  Notification.requestPermission()
+  Notification.requestPermission();
 }
 export interface Props {}
 
-let clearTimer: () => void
+let clearTimer: () => void;
 
 function startTimer(data: { loop: boolean; time: number }) {
-  store.lastState.set(data)
-  let set = data.loop ? setInterval : setTimeout
-  let clear = data.loop ? clearInterval : clearTimeout
-  clearTimer && clearTimer()
-  let start = Date.now()
+  store.lastState.set(data);
+  let set = data.loop ? setInterval : setTimeout;
+  let clear = data.loop ? clearInterval : clearTimeout;
+  clearTimer && clearTimer();
+  let start = Date.now();
   let timer = set(() => {
-    self.postMessage(JSON.stringify({ type: 'log', data: 'noti' }))
-    console.log('noti')
+    self.postMessage(JSON.stringify({ type: "log", data: "noti" }));
+    console.log("noti");
     try {
       navigator.serviceWorker
-        .getRegistration('./sw.js')
+        .getRegistration("./sw.js")
         .then((reg) => {
           reg?.showNotification(
-            '倒计时提醒：' + `${data.loop ? '循环' : ''}${data.time}分钟`,
+            "倒计时提醒：" + `${data.loop ? "循环" : ""}${data.time}分钟`,
             {}
-          )
+          );
         })
         .catch((err) => {
-          self.postMessage(JSON.stringify({ type: 'log', data: err.message }))
-        })
+          self.postMessage(JSON.stringify({ type: "log", data: err.message }));
+        });
     } catch (error: any) {
-      self.postMessage(JSON.stringify({ type: 'log', data: error.message }))
+      self.postMessage(JSON.stringify({ type: "log", data: error.message }));
     }
     try {
       const audio = new Audio(
-        `${process.env.NODE_ENV === 'development' ? '' : '/webtools'}/noti.mp3`
-      )
-      audio.load()
-      audio.play()
+        `${process.env.NODE_ENV === "development" ? "" : "/webtools"}/noti.mp3`
+      );
+      audio.load();
+      audio.play();
     } catch (error: any) {
-      self.postMessage(JSON.stringify({ type: 'log', data: error.message }))
+      self.postMessage(JSON.stringify({ type: "log", data: error.message }));
     }
-  }, data.time * 1000 * 60)
-  clearTimer = () => clear(timer)
+  }, data.time * 1000 * 60);
+  clearTimer = () => clear(timer);
 }
 
 export function Timer(props: Props) {
-  const lastState = store.lastState.get({ loop: false, time: 3 })
-  const [refresh, setRefresh] = useState(0)
-  const [time, setTime] = useState(lastState.time)
-  const [start, setStart] = useState(0)
-  const [state, setState] = useState<'ready' | 'start' | 'end'>('ready')
-  const [loop, setLoop] = useState(lastState.loop)
-  const [log, setLog] = useState<string[]>([])
+  const lastState = store.lastState.get({ loop: false, time: 3 });
+  const [refresh, setRefresh] = useState(0);
+  const [time, setTime] = useState(lastState.time);
+  const [showTimePicker, setShowTimePicker] = useState(false);
+  const [start, setStart] = useState(0);
+  const [state, setState] = useState<"ready" | "start" | "end">("ready");
+  const [loop, setLoop] = useState(lastState.loop);
+  const [log, setLog] = useState<string[]>([]);
   // const [remain, setRemain] = useState(time * 60 *1000)
   const remain =
     time * 60 * 1000 -
-    (state === 'ready'
+    (state === "ready"
       ? 0
-      : state === 'start'
+      : state === "start"
       ? (Date.now() - start) % (time * 60 * 1000)
-      : state === 'end'
+      : state === "end"
       ? time * 60 * 1000
-      : 0)
+      : 0);
   useEffect(() => {
     let t = setInterval(() => {
-      setRefresh((s) => s + 1)
-      if (remain <= 1000 && state !== 'end' && !loop) {
-        setState('end')
+      setRefresh((s) => s + 1);
+      if (remain <= 1000 && state !== "end" && !loop) {
+        setState("end");
       }
-    }, 100)
-    return () => clearInterval(t)
-  }, [remain, state, loop])
+    }, 100);
+    return () => clearInterval(t);
+  }, [remain, state, loop]);
   useEffect(() => {
-    document.title = '计时器'
-    self.addEventListener('message', (e) => {
-      if (typeof e.data === 'string' && e.data[0] === '{') {
-        let data = JSON.parse(e.data)
-        if (data.type === 'log') {
-          setLog((l) => l.concat(data.data))
+    document.title = "计时器";
+    self.addEventListener("message", (e) => {
+      if (typeof e.data === "string" && e.data[0] === "{") {
+        let data = JSON.parse(e.data);
+        if (data.type === "log") {
+          setLog((l) => l.concat(data.data));
         }
       }
-    })
-  }, [])
+    });
+  }, []);
 
   return (
     <div>
-      <div>
-        <div>{dayjs(remain - 8 * 3600 * 1000).format('HH:mm:ss')}</div>
-        <select
-          value={time}
-          onChange={(e) => {
-            setTime(Number(e.target.value))
-            setState('ready')
-          }}
-        >
-          <option value="1">1:00</option>
-          <option value="3">3:00</option>
-          <option value="5">5:00</option>
-          <option value="10">10:00</option>
-          <option value="15">15:00</option>
-          <option value="0.1">0:06</option>
-        </select>
-        <label
-          htmlFor="
-        "
-        >
-          <input
-            type="checkbox"
-            checked={loop}
-            onChange={(e) => setLoop((e.target as any).checked)}
+      <div className={css.root}>
+        <div className={css.remainCard}>
+          {dayjs(remain - 8 * 3600 * 1000).format("HH:mm:ss")}
+        </div>
+        <div className={css.props}>
+          <Button color="primary" onClick={(e) => setShowTimePicker(true)}>
+            {time + "分钟"}
+          </Button>
+          <Picker
+            visible={showTimePicker}
+            value={[time + ""]}
+            columns={[
+              [
+                { label: "1分钟", value: "1" },
+                { label: "3分钟", value: "3" },
+                { label: "4分钟", value: "4" },
+                { label: "10分钟", value: "10" },
+                { label: "15分钟", value: "15" },
+                { label: "6秒", value: "0.1" },
+              ],
+            ]}
+            onClose={() => setShowTimePicker(false)}
+            onConfirm={(e) => {
+              setTime(Number(e));
+              setState("ready");
+              setShowTimePicker(false);
+            }}
           />
-          循环
-        </label>
-        <br />
-        <button
-          onClick={(e) => {
-            startTimer({
-              loop,
-              time,
-            })
-            setStart(Date.now())
-            setState('start')
-          }}
-        >
-          开始
-        </button>
-        <button
-          onClick={(e) => {
-            clearTimer()
-            setState('end')
-          }}
-        >
-          结束
-        </button>
+          <Checkbox checked={loop} onChange={(e) => setLoop(e)}>
+            循环
+          </Checkbox>
+        </div>
+        <div className={css.props}>
+          <Button
+            color="primary"
+            onClick={(e) => {
+              startTimer({
+                loop,
+                time,
+              });
+              setStart(Date.now());
+              setState("start");
+            }}
+          >
+            开始
+          </Button>
+          <Button
+            color="warning"
+            onClick={(e) => {
+              clearTimer();
+              setState("end");
+            }}
+          >
+            结束
+          </Button>
+        </div>
       </div>
       <br />
       <br />
       <br />
       <br />
-      {location.search.includes('debug') && (
+      {location.search.includes("debug") && (
         <div>
           {log.map((l) => (
             <div>{l}</div>
@@ -147,5 +158,5 @@ export function Timer(props: Props) {
         </div>
       )}
     </div>
-  )
+  );
 }
